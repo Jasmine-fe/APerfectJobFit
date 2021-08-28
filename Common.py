85d9cf97fcf44c86518c0f77e9943486b039058f
Jasmine <jasmine880809@gmail.com>
2021-08-28 11:21:34 +0800

    [ Keyword ] optimize by unique, similairty

---
+++ Common.py
@@ -0,0 +1,48 @@
+import os
+import difflib
+import pdfplumber
+import pandas as pd
+from operator import itemgetter
+from itertools import groupby
+
+
+def getFileNameInDirectory(directoryPath):
+    fileNameList = os.listdir(directoryPath)
+    return fileNameList
+
+
+def extractPDFContent(filePath):
+    pdf = pdfplumber.open(filePath)
+    page = pdf.pages[0]
+    text = page.extract_text(x_tolerance=3)
+    return text
+
+
+def groupbyFirstLetter(iteratorSeq):
+    groupbyData = [
+        list(words) for letter, words in groupby(sorted(iteratorSeq), key=itemgetter(0))
+    ]
+    return groupbyData
+
+
+def getselectedAndUniqueKeywords(keywordDf):
+    selectedKeywords = (
+        pd.concat([keywordDf["snowball_stemmer"], keywordDf["wordnet_lemmatizer"]])
+        .sort_values()
+        .values.tolist()
+    )
+    selectedAndUniqueKeywords = set(selectedKeywords)
+    return selectedAndUniqueKeywords
+
+
+def filterSimilarityWords(inputList, thresholdRatio=0.7):
+    li = []
+    if len(inputList) > 0:
+        li = [inputList[0]]
+        for word in inputList:
+            for liWord in li:
+                ratio = difflib.SequenceMatcher(None, word, liWord).ratio()
+                if ratio < thresholdRatio:
+                    li.append(word)
+                    break
+    return li
